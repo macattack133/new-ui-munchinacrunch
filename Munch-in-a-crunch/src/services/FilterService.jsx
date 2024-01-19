6211d0d6d44193b75401550b461515d557f5cb0d
noah.brandwein1 <noah.brandwein98@gmail.com>
2024-01-18 18:14:08 -0600

    FilterService

---
+++ Munch-in-a-crunch/src/services/FilterService.jsx
@@ -0,0 +1,19 @@
+const BASE_URL = '/api/restaurants';
+
+const FilterService = {
+  getFilteredRestaurants: async (qualities) => {
+    try {
+      const response = await fetch(`${BASE_URL}/filtered${qualities ? `?qualities=${qualities.join(',')}` : ''}`);
+      
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error('Failed to fetch filtered restaurants');
+      }
+    } catch (error) {
+      throw new Error(`Error in FilterService: ${error.message}`);
+    }
+  },
+};
+
+export default FilterService;
