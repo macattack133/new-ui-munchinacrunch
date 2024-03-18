85cfd6858df8d60cb143c5dafed30488ce471fc7
mcalhoun133 <mcalhoun133@gmail.com>
2024-03-18 14:07:39 -0700

    Attempting env file API calls

--- Munch-in-a-crunch/src/components/NavigationBar.jsx
+++ Munch-in-a-crunch/src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 
 
@@ -6,24 +6,32 @@ import axios from 'axios';
 const NavigationBar = ({ onSearchResults }) => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  const handleSearch = async () => {
-    try {
-      const apiUrl = 'https://api.yelp.com/v3/businesses/search';
-      const apiKey = Config.yelpApiKey;
 
-      const response = await axios.get(apiUrl, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`,
-        },
-        params: {
-          term: searchTerm,
-          location: 'St. Louis',
-        },
-      });
+  const handleSearch = async () => {
+ try{
+
+  useEffect(() => {
+  fetch(`https://api.yelp.com/v3/businesses/search?key=${process.env.REACT_APP_API_KEY}&location=Missouri`)
+  .then(res => res.text())
+  .then(data => setSearchTerm(data));
+  });
+//     try {
+//       const apiUrl = 'https://api.yelp.com/v3/businesses/search';
+//       const apiKey =?
+//
+//       const response = await axios.get(apiUrl, {
+//         headers: {
+//           Authorization: `Bearer ${apiKey}`,
+//         },
+//         params: {
+//           term: searchTerm,
+//           location: 'Missouri',
+//         },
+//       });
       onSearchResults(response.data.businesses);
     } catch (error) {
       console.error('Error fetching data:', error);
-    }
+
   };
 
   return (
